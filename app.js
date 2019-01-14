b689f37801a4d6623bd3f2b67fababd370c7eb7a
MrKey <miftah1997mustapha@gmail.com>
2019-01-14 23:57:53 +0100

    Update writings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var pannels = document.querySelectorAll('.pannel');
 
 function active(){
+  pannels.forEach(p => p.classList.remove("active"));
   this.classList.toggle("active");
   console.log("ok");
 }
